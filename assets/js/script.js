1d294be9ed07b9b769e351a1527a85834921cd22
GenericName192 <David-barton192@hotmail.co.uk>
2025-06-11 13:21:44 +0100

    add event listeners for document loading and button clicks and fix a mistype in the index file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,20 @@
+// wait for Dom to finish loading before running the game
+// get the button elements and add event listeners to them
+document.addEventListener("DOMContentLoaded", function () {
+  let buttons = document.getElementsByTagName("button");
+
+  for (let button of buttons) {
+    button.addEventListener("click", function () {
+      if (this.getAttribute("data-type") === "submit") {
+        alert("You clicked the submit button!");
+      } else {
+        let gameType = this.getAttribute("data-type");
+        alert(`You clicked button: ${gameType}`);
+      }
+    });
+  }
+});
+
 function runGame() {}
 
 function checkAnswer() {}
