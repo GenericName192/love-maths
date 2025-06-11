86913552237a8c95ffbd9f7cafd5176eee1ea975
GenericName192 <David-barton192@hotmail.co.uk>
2025-06-11 14:13:56 +0100

    refactor operator handling in calculateCorrectAnswer and implement display functions for subtraction and multiplication

--- assets/js/script.js
+++ assets/js/script.js
@@ -70,8 +70,8 @@ function calculateCorrectAnswer() {
     case "+":
       return [operand1 + operand2, "addition"];
     case "-":
-      return [operand1 - operand2, "subtraction"];
-    case "*":
+      return [operand1 - operand2, "subtract"];
+    case "x":
       return [operand1 * operand2, "multiply"];
     case "/":
       return [operand1 / operand2, "division"];
@@ -105,8 +105,22 @@ function displayAdditionQuestion(operand1, operand2) {
   document.getElementById("operator").textContent = "+";
 }
 
-function displaySubtractQuestion() {}
+function displaySubtractQuestion(operand1, operand2) {
+  document.getElementById("operand1").textContent =
+    operand1 > operand2 ? operand1 : operand2;
+  document.getElementById("operand2").textContent =
+    operand1 > operand2 ? operand2 : operand1;
+  document.getElementById("operator").textContent = "-";
+}
 
-function displayMultiplyQuestion() {}
+function displayMultiplyQuestion(operand1, operand2) {
+  document.getElementById("operand1").textContent = operand1;
+  document.getElementById("operand2").textContent = operand2;
+  document.getElementById("operator").textContent = "x";
+}
 
-function displayDivisionQuestion() {}
+function displayDivisionQuestion(operand1, operand2) {
+  //   document.getElementById("operand1").textContent = operand1;
+  //   document.getElementById("operand2").textContent = operand2;
+  //   document.getElementById("operator").textContent = "/";
+}
